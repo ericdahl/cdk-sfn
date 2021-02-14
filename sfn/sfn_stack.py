4c56054d3e359b6283fdb671ed157ae7119fcb9e
Eric Dahl <eric.c.dahl@gmail.com>
2021-02-14 12:40:56 -0800

    fix path error and cleanup

--- sfn/sfn_stack.py
+++ sfn/sfn_stack.py
@@ -1,5 +1,3 @@
-from aws_cdk import core
-import aws_cdk.aws_ec2 as ec2
 import aws_cdk.core as core
 import aws_cdk.aws_stepfunctions as stepfunctions
 import aws_cdk.aws_stepfunctions_tasks as tasks
@@ -11,28 +9,29 @@ class SfnStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # stepfunctions.Wait(self, "Wait", stepfunctions.WaitTime.duration(core.Duration.seconds(5)))
-
-        flip_coin = lambda_.Function(self,
-                                     "FlipCoin",
-                                     runtime=lambda_.Runtime.PYTHON_3_8,
-                                     handler="index.handler",
-                                     code=lambda_.Code.from_asset("./sfn/lambda/flip_coin"))
+        flip_coin_function = lambda_.Function(self,
+                                              "FlipCoinFunction",
+                                              runtime=lambda_.Runtime.PYTHON_3_8,
+                                              handler="index.handler",
+                                              code=lambda_.Code.from_asset("./sfn/lambda/flip_coin"))
 
         flip_coin_invoke = tasks.LambdaInvoke(self,
-                                              "FlipCoinInvoke",
-                                              lambda_function=flip_coin)
+                                              "FlipCoin",
+                                              lambda_function=flip_coin_function)
 
         wait = stepfunctions.Wait(self,
                                   "Wait",
                                   time=stepfunctions.WaitTime.duration(core.Duration.seconds(5)))
 
+        tails_result = stepfunctions.Pass(self, "TailsResult")
+        tails_result.next(flip_coin_invoke)
+
         choice = stepfunctions.Choice(self,
                                       "HeadsTailsChoice") \
-            .when(condition=stepfunctions.Condition.string_equals("$.input.Payload.result", "heads"),
-                  next=stepfunctions.Succeed(self, "Succeed")) \
-            .when(condition=stepfunctions.Condition.string_equals("$.input.Payload.result", "tails"),
-                  next=flip_coin_invoke)
+            .when(condition=stepfunctions.Condition.string_equals("$.Payload.result", "heads"),
+                  next=stepfunctions.Succeed(self, "HeadsResult")) \
+            .when(condition=stepfunctions.Condition.string_equals("$.Payload.result", "tails"),
+                  next=tails_result)
 
         stepfunctions.StateMachine(self,
                                    "StateMachine",
